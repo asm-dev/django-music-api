53fb051dc7a53f18eeb76b0ed7c34ee93ef1dca1
asm-dev <anasalamancamontero@gmail.com>
2025-02-05 20:17:20 +0000

    create user and song models

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,17 @@
+from django.db import models 
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    favorite_songs = models.ManyToManyField('Song', blank=True)
+    class Meta:
+        verbose_name = "Usuario"
+        verbose_name_plural = "Usuarios"
+
+class Song(models.Model):
+    title = models.CharField(max_length=255, blank=False)
+    artist = models.CharField(max_length=255, blank=True)
+    album = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
