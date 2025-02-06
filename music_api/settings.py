98f1932167b9a6cdbc4a797b36e4090a0ae7353a
asm-dev <anasalamancamontero@gmail.com>
2025-02-06 16:40:38 +0000

    tweaw rest framework auth settings

--- music_api/settings.py
+++ music_api/settings.py
@@ -85,9 +85,13 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
 
 AUTH_USER_MODEL = 'songs.User'
